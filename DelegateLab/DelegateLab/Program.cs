f19c7b7c40247b8cffb82f52a99d1ad70020ea52
shweta <121001392+shwetaTh@users.noreply.github.com>
2024-05-28 10:45:24 +0545

    generics and delegate example

---
+++ DelegateLab/DelegateLab/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+delegate int AddDelegate(int a, int b);
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AddDelegate addDelegate = delegate (int x, int y)
+        {
+            return x + y;
+        };
+        Console.Write("Enter first number: ");
+        int num1 = int.Parse(Console.ReadLine());
+
+        Console.Write("Enter second number: ");
+        int num2 = int.Parse(Console.ReadLine());
+        int result = addDelegate(num1, num2);
+        Console.WriteLine($"Result of adding {num1} and {num2} is: {result}");
+    }
+}
