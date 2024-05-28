6cdeebc6baafc8dff6b5552c014d56b5c0ec06cb
shweta <121001392+shwetaTh@users.noreply.github.com>
2024-05-28 10:42:09 +0545

    multiple inheritance using interfaces

---
+++ InterfaceImplementLab/InterfaceImplementLab/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+interface IMyInterface
+{
+    void MyMethod();
+}
+class MyClass : IMyInterface
+{
+    public void MyMethod()
+    {
+        Console.WriteLine("Implementation of MyMethod in MyClass");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        MyClass myObject = new MyClass();
+        myObject.MyMethod();
+    }
+}
