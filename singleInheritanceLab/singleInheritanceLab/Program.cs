c58e137fb711a41d550a724f2969b78f199d779c
shweta <121001392+shwetaTh@users.noreply.github.com>
2024-05-28 10:40:48 +0545

    example of single inheritance

---
+++ singleInheritanceLab/singleInheritanceLab/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+class Animal
+{
+    public void Eat()
+    {
+        Console.WriteLine("Animal is eating");
+    }
+}
+class Dog : Animal
+{
+    public void Bark()
+    {
+        Console.WriteLine("Dog is barking");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Dog dog = new Dog();
+        dog.Eat();
+        dog.Bark();
+    }
+}
