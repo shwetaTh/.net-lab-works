189b47f711b5149190cafb28e962ac7c80ed0849
shweta <121001392+shwetaTh@users.noreply.github.com>
2024-05-28 10:43:08 +0545

    lambda expression lab work upload

---
+++ LambdaLab/LambdaLab/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<string> fruits = new List<string> { "apple", "banana", "apricot", "orange", "avocado" };
+        List<string> result = fruits.Where(fruit => fruit.StartsWith("a")).ToList();
+        Console.WriteLine("Fruits whose names start with 'a':");
+        foreach (string fruit in result)
+        {
+            Console.WriteLine(fruit);
+        }
+    }
+}
